2580ef568ad1d67b85d917a8460d0aa1cb347a0a
DESKTOP-MLT2JH8\kakao <skpark8908@gmail.com>
2020-12-13 20:40:48 +0900

    'test

--- myapi/settings.py
+++ myapi/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'myapi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASE['defalut'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
